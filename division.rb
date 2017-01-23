379052ce2e953557df022b3d65fb465e00fefa30
Tim Taylor <ttaylor@clarion.edu>
2017-01-23 11:02:29 -0500

    test dividing numbers

---
+++ division.rb
@@ -0,0 +1,4 @@
+def division(num1,num2)
+	num1 / num2
+
+end
---
+++ test_division.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'division.rb' 
+
+class TestMMFunctions < Minitest::Test 
+
+	def test_one_equals_one
+		assert_equal(1,1)
+	end
+
+	def test_10_dividedby_2_equals_5
+		assert_equal(5, division(10,2))
+	end
+
+		def test_100_dividedby_25_equals_4
+		assert_equal(4, division(100,25))
+	end
+	
+	def test_10_dividedby_0
+		assert_equal(10, division(10,0))
+	end
+
+end
